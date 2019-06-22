8a67383f5a6db95d13a24bc0e7246306d2b8f165
pipiscrew <pipiscrew@gmail.com>
2019-06-22 22:02:32 +0000

    Update postserverdatasource.ts

--- angular8_with_php_canactivate_+_retain_PHPSessionCookie/src/app/postserverdatasource.ts
+++ angular8_with_php_canactivate_+_retain_PHPSessionCookie/src/app/postserverdatasource.ts
@@ -4,7 +4,6 @@ import { ServerDataSource } from 'ng2-smart-table';
 export class PostServerDataSource extends ServerDataSource {
 
     protected requestElements(): Observable<any> {
-        console.log("sdfsd");
         let httpParams = this.createRequesParams();
         return this.http.post(this.conf.endPoint, httpParams, { withCredentials: true, observe: 'response' });
     }
